1e388100ab83dbf63547221e6b786769005e867b
Luis Angel <angel.quispe.navarro@outlook.com>
2025-03-20 23:59:35 -0500

    ItemList

--- ecommerce-tytopets/src/components/pages/itemlistcontainer/ItemListContainer.jsx
+++ ecommerce-tytopets/src/components/pages/itemlistcontainer/ItemListContainer.jsx
@@ -4,6 +4,7 @@ import { ProductCard } from '../../common/productcard/ProductCard'
 export const ItemListContainer = () => {
   return (
     <div>
+      <h2>Mis Productos</h2>
       <ProductCard title="titulo 1" price="precio 1" />
     </div>
   )
