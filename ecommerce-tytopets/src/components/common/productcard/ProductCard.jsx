b3403961fef6fcad4b01a652b4806ec8bea62b5a
Luis Angel <angel.quispe.navarro@outlook.com>
2025-03-22 22:46:08 -0500

    Funcionalidad con productCard

--- ecommerce-tytopets/src/components/common/productcard/ProductCard.jsx
+++ ecommerce-tytopets/src/components/common/productcard/ProductCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./productcard.css";
 
-export const ProductCard = ({ title, price }) => {
+export const ProductCard = ({ item }) => {
   return (
     <div className="container-card">
-      <h3> {title} </h3>
-      <h3> {price} </h3>
+      <img style={{width:"100px"}} src={item.imageUrl} alt={item.title} />
+      <h3> {item.id} </h3>
+      <h3> {item.title} </h3>
+      <h3> {item.price} </h3>
     </div>
   );
 };
