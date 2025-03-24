b528bafb8a56c5e4707524a50b6d83a7f0362179
Luis Angel <angel.quispe.navarro@outlook.com>
2025-03-23 20:32:27 -0500

    Variables de Entorno

--- ecommerce-tytopets/src/firebaseConfig.js
+++ ecommerce-tytopets/src/firebaseConfig.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyB1jl3SaCnc_aeRRDqE_Ejm0Ko2fb7vHh8",
-  authDomain: "tytopets-backend.firebaseapp.com",
-  projectId: "tytopets-backend",
-  storageBucket: "tytopets-backend.firebasestorage.app",
-  messagingSenderId: "550257349690",
-  appId: "1:550257349690:web:242fcf1378ae38038f9103"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
