d3194d9031844ae49bdc8057c4895128a49be6bc
Luis Angel <angel.quispe.navarro@outlook.com>
2025-06-10 23:43:43 -0500

    Update Footer

--- ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
+++ ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
@@ -3,7 +3,13 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <div className="spacefooter">
-      <h2>Mis Redes Sociales</h2>
+      <h2>TyTo Pets</h2>
+      <h3>Mis Redes Sociales</h3>
+      <div className="flex flex-row ">
+      <p>Youtube</p>
+      <p>Facebook</p>
+      <p>Instagram</p>
+      </div>
     </div>
   )
 }
