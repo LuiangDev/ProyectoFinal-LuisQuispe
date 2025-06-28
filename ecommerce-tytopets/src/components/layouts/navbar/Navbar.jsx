85e5894736b99db438dc42b69e9b95b9d942655a
Luis Angel <angel.quispe.navarro@outlook.com>
2025-06-27 23:58:56 -0500

    Update Navbar

--- ecommerce-tytopets/src/components/layouts/navbar/Navbar.jsx
+++ ecommerce-tytopets/src/components/layouts/navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router";
 import { CartWidget } from "../../common/cartwidget/CartWidget";
-import "./Navbar.css";
 
 export const Navbar = () => {
   return (
     <div>
-      <nav className="title">
+      <nav className="bg-yellow-500 text-white flex justify-between items-center p-4">
       <Link to="/">TyTo Pets </Link>
-        <ul className="title">
+        <ul className="flex gap-4">
           <Link to="/category/Alimentos"><li>Alimentos</li></Link>
           <Link to="/category/Accesorios"><li>Accesorios</li></Link>
           <Link to="/category/Higiene"><li>Higiene</li></Link>
