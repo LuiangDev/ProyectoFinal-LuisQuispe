0c64264f4115e50045842adb6c30048f73ed0acc
Luis Angel <angel.quispe.navarro@outlook.com>
2025-06-27 23:54:18 -0500

    Styled of footer

--- ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
+++ ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
@@ -2,10 +2,10 @@ import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <div className="spacefooter">
+    <div className="bg-sky-900 text-white p-4 flex flex-col items-center">
       <h2>TyTo Pets</h2>
       <h3>Mis Redes Sociales</h3>
-      <div className="flex flex-row ">
+      <div className="flex flex-row gap-4 ">
       <p>Youtube</p>
       <p>Facebook</p>
       <p>Instagram</p>
