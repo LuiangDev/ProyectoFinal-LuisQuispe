bde6a83cb284d6f4bb491f3005bd56529167138a
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-02 23:49:27 -0500

    Update style of footer

--- ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
+++ ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
@@ -2,10 +2,10 @@
 export const Footer = () => {
   return (
     <div className="flex felx-row justify-between items-center bg-sky-900 text-white p-4">
-      <h2>TyTo Pets</h2>
+      <h2 className="font-semibold">TyTo Pets</h2>
       <div className="bg-sky-900 text-white p-4 flex flex-col items-center">
-        <h3>Mis Redes Sociales</h3>
-        <div className="flex flex-row gap-4 ">
+        <h3 className="font-semibold">Mis Redes Sociales</h3>
+        <div className="flex flex-row gap-4 font-normal ">
           <p>Youtube</p>
           <p>Facebook</p>
           <p>Instagram</p>
