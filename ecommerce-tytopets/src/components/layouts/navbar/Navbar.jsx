71101025b2b6b70b109786245a167725d3b6e2ba
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-02 23:47:50 -0500

    Update style of navbar

--- ecommerce-tytopets/src/components/layouts/navbar/Navbar.jsx
+++ ecommerce-tytopets/src/components/layouts/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ export const Navbar = () => {
   return (
     <div>
       <nav className="bg-yellow-500 text-white flex justify-between items-center p-4">
-      <Link to="/">TyTo Pets </Link>
-        <ul className="flex gap-4 text-lg font-semibold">
+      <Link className="font-bold" to="/">TyTo Pets </Link>
+        <ul className="flex gap-4 text-base font-medium">
           <Link to="/category/Alimentos"><li>Alimentos</li></Link>
           <Link to="/category/Accesorios"><li>Accesorios</li></Link>
           <Link to="/category/Higiene"><li>Higiene</li></Link>
