d665b66f94ea12375f7ba0813ead1bf0db8f59fb
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-07 23:51:00 -0500

    Update view footer

--- ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
+++ ecommerce-tytopets/src/components/layouts/footer/Footer.jsx
@@ -1,14 +1,14 @@
-
+import { FaLinkedinIn,FaFacebookF,FaInstagram } from "react-icons/fa";
 export const Footer = () => {
   return (
-    <div className="flex felx-row justify-between items-center bg-sky-900 text-white p-4">
+    <div className="flex felx-row justify-between items-center bg-sky-900 text-white p-4 rounded-t-3xl">
       <h2 className="font-semibold">TyTo Pets</h2>
       <div className="bg-sky-900 text-white p-4 flex flex-col items-center">
-        <h3 className="font-semibold">Mis Redes Sociales</h3>
+        <h3 className="font-semibold mb-2">Mis Redes Sociales</h3>
         <div className="flex flex-row gap-4 font-normal ">
-          <p>Youtube</p>
-          <p>Facebook</p>
-          <p>Instagram</p>
+          <div className="rounded-full bg-amber-400 p-2"><FaLinkedinIn /></div>
+          <div className="rounded-full bg-amber-400 p-2"><FaFacebookF /></div>
+          <div className="rounded-full bg-amber-400 p-2"><FaInstagram /></div>
         </div>
       </div>
     </div>
