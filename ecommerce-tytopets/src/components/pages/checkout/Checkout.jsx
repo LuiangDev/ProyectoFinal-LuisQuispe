93d3ad4b09a9905435fc3ad51b6c6e8f421344cf
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-08 00:10:03 -0500

    Update view styles of Checkout

--- ecommerce-tytopets/src/components/pages/checkout/Checkout.jsx
+++ ecommerce-tytopets/src/components/pages/checkout/Checkout.jsx
@@ -44,31 +44,38 @@ const [orderId, setOrderId] = useState(null);
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "20px", padding: "20px" }}>
+    <div className="flex flex-col items-center justify-center my-8">
+      <h1 className="text-2xl font-bold">Finalizar Compra</h1>
+      <h2 className="text-lg font-medium mb-6">Por favor, completa tus datos</h2>
       {orderId ? (
         <h2 className="text-xl font-medium">Tu numero de compra es: {orderId}</h2>
       ):(
-      <form  style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "20px"}} onSubmit={funcionFormulario}>
+      <form  className="flex flex-col gap-4" onSubmit={funcionFormulario}>
         <input
           type="text"
           placeholder="nombre"
           name="nombre"
           onChange={funcionInputs}
+          className="border p-1 rounded"
         />
         <input
           type="text"
           placeholder="email"
           name="email"
           onChange={funcionInputs}
+          className="border p-1 rounded"
         />
         <input
           type="text"
           placeholder="telefono"
           name="telefono"
           onChange={funcionInputs}
+          className="border p-1 rounded"
         />
-        <button style={{padding:"3px"}} >Enviar</button>
-        <button style={{padding:"3px"}} type="button">cancelar</button>
+        <div className="flex flex-row gap-4 mt-4">
+        <button className="px-8 py-1 cursor-pointer text-white rounded-xl font-normal bg-sky-900" type="button" >Enviar</button>
+        <button className="px-8 py-1 cursor-pointer text-white rounded-xl font-normal bg-sky-900" type="button">Cancelar</button>
+        </div>
       </form>
       )}
     </div>
