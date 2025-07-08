0a826891b9ec46f5d10d7cc382b9193993c352f8
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-07 23:40:34 -0500

    Update style of product card

--- ecommerce-tytopets/src/components/common/productcard/ProductCard.jsx
+++ ecommerce-tytopets/src/components/common/productcard/ProductCard.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router";
 
 export const ProductCard = ({ item }) => {
   return (
-    <div className="container-card">
-      <img style={{ width: "100px" }} src={item.imageUrl} alt={item.title} />
+    <div className="container-card max-w-2/2 flex flex-col items-center justify-center p-4 border border-gray-300 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <img className="w-30" src={item.imageUrl} alt={item.title} />
       <h3> {item.title} </h3>
       <h3> {item.price} </h3>
-      <Link className="text-cyan-700" to={`/itemdetail/${item.id}`}>Ver detalle</Link>
+      <Link className="text-white px-4 py-1 bg-cyan-700 rounded-xl" to={`/itemdetail/${item.id}`}>Ver detalle</Link>
     </div>
   );
 };
