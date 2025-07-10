d73a0816957eaacf3133b5f88dfc9847ba3f02c7
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-09 23:40:34 -0500

    Navbar static with scroll

--- ecommerce-tytopets/src/components/layouts/navbar/Navbar.jsx
+++ ecommerce-tytopets/src/components/layouts/navbar/Navbar.jsx
@@ -3,17 +3,16 @@ import { CartWidget } from "../../common/cartwidget/CartWidget";
 
 export const Navbar = () => {
   return (
-    <div>
-      <nav className="bg-yellow-500 text-white flex justify-between items-center p-4">
-      <Link className="font-bold" to="/">TyTo Pets </Link>
+    <div className="sticky top-0 z-50">
+      <nav className="bg-yellow-500 text-white flex justify-between items-center p-4 shadow-md">
+        <Link className="font-bold text-xl" to="/">TyTo Pets</Link>
         <ul className="flex gap-4 text-base font-medium">
           <Link to="/category/Alimentos"><li>Alimentos</li></Link>
           <Link to="/category/Accesorios"><li>Accesorios</li></Link>
           <Link to="/category/Higiene"><li>Higiene</li></Link>
           <Link to="/category/CamasyTransportadoras"><li>Camas y Transportadoras</li></Link>
-
         </ul>
-        <Link to="/cart"><CartWidget /> </Link>
+        <Link to="/cart"><CartWidget /></Link>
       </nav>
     </div>
   );
