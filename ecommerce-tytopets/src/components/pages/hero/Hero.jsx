72159aaf90685db2a9818727de89bb772d5fda01
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-13 22:24:58 -0500

    Add view Hero

--- ecommerce-tytopets/src/components/pages/hero/Hero.jsx
+++ ecommerce-tytopets/src/components/pages/hero/Hero.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
-const Hero = () => {
+export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className=" bg-gray-500 p-6 mb-8">
+      <div className="flex flex-col items-center justify-center h-[400px]">
+        <h1 className="text-4xl font-bold text-white">Bienvenido a TyTo Pets</h1>
+        <p className="text-lg text-white mt-2">Todo lo que tu mascota necesita en un solo lugar</p>
+        <button className="mt-4 px-6 py-2 cursor-pointer bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition-colors">
+          Explorar Productos
+        </button>
+      </div>
+    </div>
   )
 }
-
-export default Hero
--- ecommerce-tytopets/src/components/pages/itemlistcontainer/ItemListContainer.jsx
+++ ecommerce-tytopets/src/components/pages/itemlistcontainer/ItemListContainer.jsx
@@ -1,40 +1,46 @@
 import { useEffect, useState } from "react";
 import { ProductCard } from "../../common/productcard/ProductCard";
 import { useParams } from "react-router";
+import { Hero } from "../hero/Hero";
 import { db } from "../../../firebaseConfig";
 
-import {collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export const ItemListContainer = () => {
   const [items, setItems] = useState([]);
   const { name } = useParams();
-  
-  useEffect(() => {
-let refCollection = collection(db, "products");
-let consulta=refCollection;
-if(name){
-  consulta=query(refCollection, where("category", "==", name))
-}
-const getProducts = getDocs(consulta);
 
-getProducts.then((res)=>{
-  const nuevoArray = res.docs.map((elemento) => {
-    return{id: elemento.id, ...elemento.data()};
-});
-setItems(nuevoArray);
-})
-.catch((error)=>console.log(error));
+  useEffect(() => {
+    let refCollection = collection(db, "products");
+    let consulta = refCollection;
+    if (name) {
+      consulta = query(refCollection, where("category", "==", name));
+    }
+    const getProducts = getDocs(consulta);
+
+    getProducts
+      .then((res) => {
+        const nuevoArray = res.docs.map((elemento) => {
+          return { id: elemento.id, ...elemento.data() };
+        });
+        setItems(nuevoArray);
+      })
+      .catch((error) => console.log(error));
   }, [name]);
 
   return (
-    <div className="flex flex-col items-center justify-center py-4">
-    <h2 className="font-bold text-2xl text-cyan-700 my-4">Listado de productos</h2>
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 px-2">
-
-      {items.map((item) => {
-        return <ProductCard key={item.id} item={item} />;
-      })}
-    </section>
+    <div className="flex flex-col items-center justify-center ">
+      <div className="w-full">
+        <Hero />
+      </div>
+      <h2 className="font-bold text-2xl text-cyan-700 my-4 py-4">
+        Listado de productos
+      </h2>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 px-2">
+        {items.map((item) => {
+          return <ProductCard key={item.id} item={item} />;
+        })}
+      </section>
     </div>
   );
 };
