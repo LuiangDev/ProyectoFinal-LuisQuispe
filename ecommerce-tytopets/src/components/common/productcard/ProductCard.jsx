99a50d416ee0e52e0817119090c9ff5a3dccc602
Luis Angel <angel.quispe.navarro@outlook.com>
2025-07-28 19:49:44 -0500

    Update access of ListemContainer

--- ecommerce-tytopets/src/components/common/productcard/ProductCard.jsx
+++ ecommerce-tytopets/src/components/common/productcard/ProductCard.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router";
 
 export const ProductCard = ({ item }) => {
   return (
-    <div className="container-card max-w-2/2 flex flex-col items-center justify-center p-4 border border-gray-300 rounded-lg shadow-md hover:shadow-lg  duration-300 bg-white overflow-hidden transform transition-transform hover:scale-105">
+    <Link to={`/itemdetail/${item.id}`}className="container-card max-w-2/2 flex flex-col items-center justify-center p-4 border border-gray-300 rounded-lg shadow-md hover:shadow-lg  duration-300 bg-white overflow-hidden transform transition-transform hover:scale-105">
       <img className="w-30" src={item.imageUrl} alt={item.title} />
       <h3> {item.title} </h3>
       <h3> {item.price} </h3>
-      <Link className="text-white px-4 py-1 bg-cyan-700 rounded-xl" to={`/itemdetail/${item.id}`}>Ver detalle</Link>
-    </div>
+      <button className="text-white px-4 py-1 bg-cyan-700 rounded-xl" to={`/itemdetail/${item.id}`}>Ver detalle</button>
+    </Link>
   );
 };
