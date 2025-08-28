0110bfaacfc85e05343f6479e4a9181c4f2b8ecf
Luis Angel <angel.quispe.navarro@outlook.com>
2025-08-27 22:07:44 -0500

    Update view auth

--- ecommerce-tytopets/src/components/auth/ResetPassword.jsx
+++ ecommerce-tytopets/src/components/auth/ResetPassword.jsx
@@ -21,7 +21,7 @@ const ResetPassword = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
-        <h2 className="text-2xl font-bold text-center text-primary mb-6">
+        <h2 className="text-2xl font-bold text-center text-cyan-700 mb-6">
           🔒 Restablecer Contraseña
         </h2>
 
@@ -32,7 +32,7 @@ const ResetPassword = () => {
             <Link
               type="submit"
                 to="/login"
-              className="w-full bg-blue-600 text-white cursor-pointer py-2 px-4 rounded-md transition duration-300"
+              className="w-full bg-sky-900 text-white cursor-pointer py-2 px-4 rounded-md transition duration-300"
             >
               Iniciar sesión
             </Link>
@@ -68,7 +68,7 @@ const ResetPassword = () => {
 
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white cursor-pointer py-2 px-4 rounded-md transition duration-300"
+              className="w-full bg-sky-900 text-white cursor-pointer py-2 px-4 rounded-md transition duration-300"
             >
               Restablecer contraseña
             </button>
--- ecommerce-tytopets/src/components/pages/Login.jsx
+++ ecommerce-tytopets/src/components/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-md">
-        <h1 className="text-2xl font-bold text-center text-sky-900 mb-6">Iniciar Sesión</h1>
+        <h1 className="text-3xl font-extrabold text-center text-cyan-700 mb-6">Iniciar Sesión</h1>
         <LoginForm />
       </div>
     </div>
